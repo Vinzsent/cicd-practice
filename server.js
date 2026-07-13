f8555780ec7da537b28c5f5deaf0a0d3faf6098d
Vincent Crame <vincentcrame7@gmail.com>
2026-07-13 15:30:13 +0800

    edit: res.send text

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! My CI/CD pipeline works!');
+  res.send('My First automated deployment with GitHub Actions and Render!');
 });
 
 app.listen(PORT, () => {
