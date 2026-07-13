8a3a382ebc62cf8e663e28421fab007985a74d16
Vincent Crame <vincentcrame7@gmail.com>
2026-07-13 15:32:26 +0800

    2nd commit message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('My First automated deployment with GitHub Actions and Render!');
+  res.send('My Express App is running! This is my second time deploying to Render. I have made some changes to the code and redeployed it.');
 });
 
 app.listen(PORT, () => {
